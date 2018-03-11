df12e0b880e74dc01eb0c7a93acaf61c76b68698
Andrew Gabriel <Andrew@andrewtechful.com>
2018-03-11 14:01:48 -0400

    Replacing static values with dynamic data from JSON

--- renderer.js
+++ renderer.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
 
+function mergeValues(values, content) {
+    // Cycle over keys
+    for (let key in values) {
+        // Replace all {{key}} with the value from the values object
+        content = content.replace(`{{${key}}}`, values[key]);
+    }
+    return content;
+}
+
 function view (templateName, values, res) {
     // Read from the template files
-    let fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    let fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
     // Insert values into the content
+    fileContents = mergeValues(values, fileContents);
     // Write out the contents to the reponse
     res.write(fileContents);
-    // Insert values in to the content
-    // Write out to the response
 }
 
 module.exports = { view }
